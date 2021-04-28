7338751bb4eb58b68c24c64b703ff39de5605adf
Yuterfrw771 <75987761+Yuterfrw771@users.noreply.github.com>
2021-04-28 12:00:53 +0900

    Initial commit

---
+++ Assets/ItemRemover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemRemover : MonoBehaviour
+{
+    //画面に映る可能性のあるz軸最大の場所
+    private float visiblePosZ = 0;
+    //画面に映る可能性のあるz軸最大の場所
+    private float visibleCamera = -6f;
+    //Unityちゃんのオブジェクト
+    private GameObject unitychan;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Unityちゃんのオブジェクトを取得
+        this.unitychan = GameObject.Find("unitychan");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //画面に移る範囲を計算
+        visiblePosZ = unitychan.transform.position.z + visibleCamera;
+
+        //消すための条件分岐
+        if (visiblePosZ > this.transform.position.z)
+        {
+            //アイテムを破棄する
+            Destroy(this.gameObject);
+
+        }
+    }
+}
